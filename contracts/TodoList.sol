da38420c0d12b6ab1d5c7060da6f2e521b7bb741
ManishGotame <manishgt194@gmail.com>
2021-10-13 22:37:16 +0545

    First commit

---
+++ contracts/TodoList.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+	// keep the number of the tasks in the todo list
+
+	uint public taskCount = 0; // this state variable is written directly to the blockchain
+}
